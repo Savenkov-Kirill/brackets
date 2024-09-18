b59dd506bef0f56f208cdcf645cd17d939d8b381
Kirill Savenkov <ksavenkov@mail.ru>
2024-09-18 22:46:59 +0300

    init: start task brackest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = new Set();
+  const closingBrackets = new Set();
+  const matchingBracket = {};
+
+  for (const [open, close] of bracketsConfig) {
+      openingBrackets.add(open);
+      closingBrackets.add(close);
+      matchingBracket[close] = open;
+  }
+
+  for (let char of str) {
+      if (openingBrackets.has(char)) {
+          if (stack.length > 0 && char === matchingBracket[char]) {
+              if (stack[stack.length - 1] === char) {
+                  stack.pop();
+              } else {
+                  stack.push(char);
+              }
+          } else {
+              stack.push(char);
+          }
+      } else if (closingBrackets.has(char)) {
+          if (stack.length > 0 && stack[stack.length - 1] === matchingBracket[char]) {
+              stack.pop();
+          } else {
+              return false;
+          }
+      }
+  }
+
+  return stack.length === 0;
+};
